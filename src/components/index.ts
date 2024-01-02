25d0718621b928755f40585ff919388372e4ca48
saulvg <saulvgproyecto@gmail.com>
2024-01-02 17:14:31 +0100

    add video component

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import Bento from "./Bento.astro";
+import Footer from "./Footer.astro";
+import Header from "./Header.astro";
+import Intro from "./Intro.astro";
+import PreFooter from "./PreFooter.astro";
+import Video from "./Video.astro";
